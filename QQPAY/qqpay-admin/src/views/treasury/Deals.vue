9b6d3473257e81dd49990e4b80b18223a94f2f98
moazzam023 <moazzam1311023@gmail.com>
2022-05-26 23:34:50 +0530

    deals page - API integration

--- QQPAY/qqpay-admin/src/views/treasury/Deals.vue
+++ QQPAY/qqpay-admin/src/views/treasury/Deals.vue
@@ -5,10 +5,10 @@
       <b-col>
         <div>
           <b-button-group size="sm">
-            <b-button variant="outline-light" v-b-modal.add-deal>
+            <b-button variant="outline-light" v-b-modal.add-deal @click="updateTrigger = true;">
               <b-icon
                 icon="file-earmark-plus-fill"
-                v-on:click="getContract"
+                
               ></b-icon>
               New Deal
             </b-button>
@@ -53,14 +53,13 @@
       :filter="filter"
       responsive
       class="align-middle"
-      v-for="item in list"
-      v-bind:key="item.id"
+ 
     >
       <template #cell(item)="row">
         <b>{{ row.item.deal_type }} </b>
       </template>
       <template #cell(deal_date)="row"
-        ><b>{{ row.item.deal_date }}</b>
+        ><b>{{ format(row.item.deal_date) }}</b>
       </template>
       <template #cell(actions)="row" size="sm">
         <div class="action-div">
@@ -77,8 +76,8 @@
       </template>
       <template #row-details="row">
         <b-card>
-          <b-row>
-            <b-col sm="12" md="6" lg="4">
+          <b-row align-h="between">
+            <b-col sm="12" md="6" lg="6">
               <div class="menu-sec">
                 <label class="title-lbl">Additional Info</label>
                 <b-list-group flush>
@@ -86,13 +85,13 @@
                     class="d-flex justify-content-between align-items-center"
                   >
                     <label>Contract Number</label>
-                    <label>{{ row.item.contract_no }}</label>
+                    <label>{{ row.item.deal_no }}</label>
                   </b-list-group-item>
                   <b-list-group-item
                     class="d-flex justify-content-between align-items-center"
                   >
                     <label>Purchase Date</label>
-                    <label>{{ row.item.purchase_date }}</label>
+                    <label>{{ format(row.item.purchase_date) }}</label>
                   </b-list-group-item>
 
                   <b-list-group-item
@@ -113,13 +112,13 @@
                     class="d-flex justify-content-between align-items-center"
                   >
                     <label>Bank POC</label>
-                    <label>{{ row.item.Bank_POC }}</label>
+                    <label>{{ row.item.bank_poc }}</label>
                   </b-list-group-item>
 
                   <b-list-group-item
                     class="d-flex justify-content-between align-items-center"
                   >
-                    <label>TOF</label>
+                    <label>Target of funds</label>
                     <label>{{ row.item.TOF }}</label>
                   </b-list-group-item>
 
@@ -134,27 +133,29 @@
                     class="d-flex justify-content-between align-items-center"
                   >
                     <label>Service Charge</label>
-                    <label>{{ row.item.Service_Charge }}</label>
+                    <label>{{ row.item.bank_charge }}</label>
                   </b-list-group-item>
 
                   <b-list-group-item
                     class="d-flex justify-content-between align-items-center"
                   >
                     <label>Service Tax</label>
-                    <label>{{ row.item.Service_Tax }}</label>
+                    <label>{{ row.item.tax }}</label>
                   </b-list-group-item>
                 </b-list-group>
               </div>
             </b-col>
-            <b-col sm="12" md="6" lg="6" align-h="center">
+            <!-- <b-col sm="12" md="6" lg="6" align-h="center">
               <TreasuryFlow />
-            </b-col>
-            <b-col sm="12" md="6" lg="2">
+            </b-col> -->
+            <b-col sm="12" md="6" lg="3">
               <div class="menu-sec">
                 <b-button
                   variant="outline-light"
                   size="sm"
                   class="wd-100p mb-2"
+                  @click="onclickUpdate(row.item)"
+                  v-b-modal.add-deal
                 >
                   <b-icon icon="pencil-square" aria-hidden="true"></b-icon>
                   Update
@@ -190,7 +191,16 @@
     >
       <b-card header="Deal Details" header-tag="header">
         <b-row>
-          <b-col>
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="Deal Type">
+              <!-- <b-form-input
+                v-model="temp_deal.deal_type"
+                size="sm"
+              ></b-form-input> -->
+              <b-form-select v-model="temp_deal.deal_type" :options="dealOption"></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col sm="12" md="4" lg="4">
             <b-form-group
               id="fieldset-1"
               label="Deal Date"
@@ -205,52 +215,7 @@
               </b-form-datepicker>
             </b-form-group>
           </b-col>
-          <b-col>
-            <b-form-group label="Exchange Rate">
-              <b-form-input
-                v-model="temp_deal.exchange_rate"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-
-          <b-col>
-            <b-form-group label="i_o_IRH">
-              <b-form-input
-                v-model="temp_deal.i_o_IRH"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-
-          <b-col>
-            <b-form-group label="Source of funds">
-              <b-form-input
-                v-model="temp_deal.source_of_funds"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-form-group label="USD Amount">
-              <b-form-input
-                v-model="temp_deal.amount_in_USD"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group label="MYR Amount">
-              <b-form-input
-                v-model="temp_deal.amount_in_MYR"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-
-          <b-col>
+          <b-col sm="12" md="4" lg="4">
             <b-form-group label="Deal No">
               <b-form-input
                 v-model="temp_deal.deal_no"
@@ -258,121 +223,210 @@
               ></b-form-input>
             </b-form-group>
           </b-col>
+        </b-row>
 
-          <b-col>
-            <b-form-group label="NoOf Setle">
+        <b-row>
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="USD Amount">
               <b-form-input
-                v-model="temp_deal.NoOf_Setle"
+                v-model="temp_deal.fcy_amount"
                 size="sm"
               ></b-form-input>
             </b-form-group>
           </b-col>
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="Exchange Rate">
+              <b-form-input
+                v-model="temp_deal.fcy_deal_rate"
+                size="sm"
+              ></b-form-input>
+            </b-form-group>
+          </b-col>
+
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="MYR Amount">
+              <b-form-input
+                v-model="temp_deal.lcy_amount"
+                size="sm"
+              ></b-form-input>
+            </b-form-group>
+          </b-col>
+        
+        </b-row>
+        <b-row align-h="start">
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="Source of funds">
+              <b-form-select v-model="temp_deal.source_of_funds" :options="fundSourceOption"></b-form-select>
+            </b-form-group>
+          </b-col>            
         </b-row>
       </b-card>
 
       <b-card header="Purchase Details" header-tag="header">
         <b-row>
-          <b-col>
-            <b-form-group label="Purchase Date">
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group id="purchase-date" label="Purchase Date" label-for="purchase-datepicker">
               <b-form-datepicker
+                id="purchase-datepicker"
                 v-model="temp_deal.purchase_date"
                 class="mb-2"
                 size="sm"
               ></b-form-datepicker>
             </b-form-group>
           </b-col>
-          <b-col>
-            <b-form-group label="Service Charge" label-for="input-1">
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="Bank Charge" label-for="input-1">
               <b-form-input
                 id="input-1"
-                v-model="temp_deal.Service_Charge"
+                v-model="temp_deal.bank_charge"
                 size="sm"
               ></b-form-input>
             </b-form-group>
           </b-col>
-          <b-col>
-            <b-form-group label="Service Tax">
-              <b-form-input
-                v-model="temp_deal.Service_Tax"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group label="Created By">
-              <b-form-input
-                v-model="temp_deal.created_by"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <b-row>
-          <b-col>
-            <b-form-group label="Edited By">
-              <b-form-input
-                v-model="temp_deal.edited_by"
-                size="sm"
-              ></b-form-input>
-            </b-form-group>
-          </b-col>
-
-          <b-col>
-            <b-form-group label="TOF">
+          <b-col sm="12" md="4" lg="4">
+            <b-form-group label="Target of Fund">
               <b-form-input v-model="temp_deal.TOF" size="sm"></b-form-input>
             </b-form-group>
-          </b-col>
+          </b-col>          
 
-          <b-col>
+        </b-row>
+
+        <b-row align-h="start">  
+          <b-col sm="12" md="4" lg="4">
             <b-form-group label="Bank POC">
               <b-form-input
-                v-model="temp_deal.Bank_POC"
+                v-model="temp_deal.bank_poc"
+                size="sm"
+              ></b-form-input>
+            </b-form-group>
+          </b-col>
+          <b-col  sm="12" md="4" lg="4">
+            <b-form-group label="Service Tax">
+              <b-form-input
+                v-model="temp_deal.tax"
                 size="sm"
               ></b-form-input>
             </b-form-group>
           </b-col>
 
-          <b-col>
-            <b-form-group label="Status">
-              <b-form-input v-model="temp_deal.Status" size="sm"></b-form-input>
-            </b-form-group>
-          </b-col>
         </b-row>
       </b-card>
       <b-form-textarea
         id="textarea"
-        v-model="temp_deal.comment"
+        v-model="temp_deal.remarks"
         placeholder="Remarks"
         rows="3"
         max-rows="6"
       >
       </b-form-textarea>
       <template #modal-footer="{ ok }">
-        <b-button variant="primary" @click="ok()"> SUBMIT </b-button>
+        <b-button variant="primary" @click="ok();postDealData()"> SUBMIT </b-button>
       </template>
     </b-modal>
   </div>
 </template>
 
 <script>
-import TreasuryFlow from "@/components/flow/TreasuryFlow.vue";
 import axios from "axios";
 export default {
   name: "Deals",
   components: {
-    TreasuryFlow,
+    
   },
   created() {
     this.getContract();
   },
+  computed: {
+    getAmtAndRate() {
+      return { 
+        amt: this.temp_deal.fcy_amount,
+        rate: this.temp_deal.fcy_deal_rate
+      };
+    }
+  },
+  watch: {
+    getAmtAndRate: function (newValue) {
+      console.log(newValue, 'new value')
+      if (newValue.amt >= 0 && newValue.rate >= 0) {
+        this.temp_deal.lcy_amount = (
+          newValue.amt *
+          newValue.rate
+        ).toFixed(2);
+      }
+    },
+  },
+  
   data() {
     return {
       token:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTMxMTMyNTcsInN1YiI6ImFuc2hpa2FndXB0YTU4M0BnbWFpbC5jb20ifQ.8nHju6H4hYur2lW8ejF-H_4y36gTlCetNeXi-VrZ0uE",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTM1ODg2MjMsInN1YiI6Im1vYXp6YW0xMzExMDIzQGdtYWlsLmNvbSJ9.RXuz6sKykSzMSMQodWzFWGscsNCJ4qVD41FoAc7ZT4o",
+      base_url: 'http://3.111.140.40:8001/api/v1/',
       filter: null,
+      dealsTableData: null,
+      updateTrigger: false,
+      dealOption: [
+        { value: 'i', text: 'Inward' },
+        { value: 'o', text: 'Outward' },
+        { value: 'irh', text: 'IRH' },        
+      ],
+      fundSourceOption: [
+        { value: 'type1', text: 'Fund 1' },
+        { value: 'type2', text: 'Fund 2' },        
+      ],
+/*       temp_deal: {
+        i_o_IRH: "",
+        deal_date: "",
+        source_of_funds: "",
+        amount_in_USD: "",
+        exchange_rate: "",
+        amount_in_MYR: "",
+        deal_no: "",
+        NoOf_Setle: "",
+        bank_poc: "",
+        TOF: "",
+        bank_charge: "",
+        tax: "",
+     
+        purchase_date: "",
+        status: "",
+        created_by: "",
+        break_down: "",
+        edited_by: "",
+        comment: "",
+
+      }, */
       temp_deal: {
-        list: undefined,
+        approved_aate: null,
+        approved_by: "",
+        authorization_flag: false,
+        authorized_by: "",
+        authorized_date: null,
+        bank_charge: '',
+        bank_poc: "",
+        created_by: "",
+        created_date: "",
+        deal_date: "",
+        deal_id: false,
+        deal_no: "",
+        deal_type: "",
+        del_flag: false,
+        fcy_amount: null,   // USD Amt
+        fcy_balance: null,
+        fcy_ccy: "",
+        fcy_deal_rate: '',
+        is_settled: false,
+        lcy_amount: null,
+        lcy_ccy: "",
+        pp_amount: 0,
+        purchase_date: "",
+        rate_coll_ppccy: 0,
+        remarks: "",
+        source_bank_id: "",
+        target_bank_id: "",
+        tax: null,
+        updated_by: "",
+        updated_date: null,
+        voucher_no: "",
       },
       menu_hierarchy: [
         {
@@ -385,17 +439,38 @@ export default {
         },
       ],
       fields: [
-        "i_o_IRH",
-        "deal_date",
-        "source_of_funds",
-        "amount_in_USD",
-        "exchange_rate",
-        "amount_in_MYR",
-        "status",
+        {
+          key: 'deal_type',
+          label: 'Deal Type',
+        },
+        {
+          key: 'deal_date',
+          label: 'Deal Date',
+        },
+        {
+          key: 'source_of_funds',
+          label: 'Source of Funds',
+        },
+        {
+          key: 'fcy_amount',
+          label: 'Amount in USD',
+        },
+        {
+          key: 'fcy_deal_rate',
+          label: 'Exchange Rate',
+        },
+        {
+          key: 'lcy_amount',
+          label: 'Amount in MYR',
+        },
+        {
+          key: 'status',
+          label: 'Status',
+        },
         { key: "actions", label: "" },
       ],
       items: [
-        {
+        /* {
           i_o_IRH: "O",
           deal_date: "01 Nov 21",
           source_of_funds: "Maybank",
@@ -407,14 +482,14 @@ export default {
           status: "Open",
           created_by: "Siva",
           NoOf_Setle: "1",
-          Bank_POC: "",
+          bank_poc: "",
           TOF: "",
-          Service_Charge: "",
-          Service_Tax: "",
+          bank_charge: "",
+          tax: "",
           break_down: "2",
           edited_by: "",
           comment: "This is a sample Deal",
-        },
+        }, */
       ],
     };
   },
@@ -425,24 +500,83 @@ export default {
     nav_update() {
       console.log("nav update");
     },
+    postDealData() {
+      const request = this.getRequest();
+      // console.log('req', JSON.parse(JSON.stringify(request)))
+      if (this.updateTrigger) {
+        axios.put(this.base_url + "new-contract/update-contract/" + request.deal_id, request, {
+            headers: {
+              Authorization: `Token ${this.token}`,
+            },
+          })
+          .then((response) => {
+            console.log(response.data.data);
+            const index = this.items.findIndex(ele => ele.deal_id === this.temp_deal.deal_id);
+            this.items[index] = this.temp_deal;
+          })
+          .catch((err) => {
+            console.log('Deal not posted', err);
+        });
+
+      } else {
+        axios.post(this.base_url + "new-contract/new-contract", request, {
+            headers: {
+              Authorization: `Token ${this.token}`,
+            },
+          })
+          .then((response) => {
+            console.log(response.data.data)
+            this.items.push(this.temp_deal);
+          })
+          .catch((err) => {
+            console.log('Deal not posted', err);
+        });
+        
+      }
+    },
+    getRequest() {
+      const req = this.temp_deal;
+      for (const key of Object.keys(req)) {
+        console.log(key, req[key]);
+        if (key === 'deal_date' || key === 'purchase_date' || key === 'updated_date' || key === 'approved_aate' || key === 'authorized_date') {
+          if (req[key]) {
+            req[key] = new Date(req[key]);
+          }
+        }
+      }
+      return req;
+    },
 
     async getContract() {
       console.log("token", this.token);
 
       axios
-        .get("http://3.111.140.40:8001/api/v1/new-contract/get-all-contract", {
+        .get(this.base_url + "new-contract/get-all-contract", {
           headers: {
             Authorization: `Bearer ${this.token}`,
           },
         })
-        .then((response) => {
-          this.list = response.data.data;
-          console.log("result", response.data.data);
+        .then(response => {
+          // this.dealsTableData = JSON.parse(response.data.data);
+          this.items = JSON.parse(JSON.stringify(response.data.data[0]));
+          console.log("this.items", this.items);
         })
         .catch((e) => {
           console.log(e);
         });
     },
+    format(date) {
+      if (date) {
+        date = new Date(date);
+        let month = date.toLocaleString("en-US", { month: 'short' })
+        return date.getDate() + ' ' + month + ' ' + date.getFullYear();
+      }
+    },
+    onclickUpdate(selectedRow) {
+      this.updateTrigger = true;
+      console.log('slected', selectedRow)
+      this.temp_deal = selectedRow;
+    }
   },
 };
 
